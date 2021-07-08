bfd873579f6ac149d398b7eee5100ee069bad6d9
schalliiii <s.schallmaier@gmx.de>
2021-07-08 23:54:31 +0200

    Fix double player1

--- canvas.js
+++ canvas.js
@@ -1,7 +1,5 @@
 var canvas = document.getElementById('pitch');
 var ctx = canvas.getContext('2d');
-var aimX;
-var aimY;
 var power = 1;
 class player {
   constructor(number, team, speed, startingX, startingY, accuracy, possession, trickotColor){
@@ -17,26 +15,39 @@ class player {
       this.trickotColor = trickotColor;
   }
   draw(ctx) {
+    console.log(ctx);
       ctx.beginPath();
-      ctx.arc(this.x, this.y, 6, 0, 2*Math.PI, false);
-      ctx.fillStyle = this.trickotColor;
-      ctx.fill();
-      ctx.strokeStyle = "#000";
+      ctx.strokeStyle = this.trickotColor;
+      ctx.arc(this.x, this.y, 10, 0, 2*Math.PI, false);
+      ctx.font = '8pt Calibri';
       ctx.stroke();
+      ctx.textAlign = "center";
+      ctx.fillText(this.number, this.x, this.y);
       ctx.closePath(); 
   }
 
   isAt(ballX, ballY){
+    this.possession = true;
       return Math.abs(this.x - ballX) < 1 ? (Math.abs(this.y - ballY) < 1 ? true : false) : false;
   }
   
-  move(ballX, ballY){
+  move(ballX, ballY, ctx){
     var bX = ballX,
         bY = ballY;
       if (!this.isAt(bX, bY)) {
+        if(bX > this.x){
           this.x = this.x + this.speed;
+        }
+        if(bX < this.X){
+          this.x = this.x - this.speed;
+        }
+        if(bY > this.Y){
           this.y = this.y + this.speed;
-          this.draw();
+        }
+        if(bY < this.Y){
+          this.y = this.y - this.speed;
+        }
+          this.draw(ctx);
         }
   }
 
@@ -93,6 +104,13 @@ class ball{
     getY(){
       return this.y;
     }  
+    setX(x){
+      this.x = x;
+    }
+    setY(y){
+      this.y = y;
+    }  
+    
 }
 
 class referee{
@@ -126,18 +144,19 @@ getY(){
   return this.y;
 }
 }
+var playball = new ball();
 var oneH = new player(1, "home", 2, 180, 100, 100, false, "#0047AB");
 var twoH = new player(2, "home", 2, 140, 260, 100, false, "#0047AB");
 var threeH = new player(3, "home", 2, 320, 60, false, "#0047AB");
 var fourH = new player(4, "home", 2, 285, 250, 100, false, "#0047AB");
-var fiveH = new player(5, "home", 2, 260, 410, 100, false, "#0047AB");
+var fiveH = new player(5, "home", 2, 280, 440, 100, false, "#0047AB");
 var sixH = new player(6, "home", 2, 500, 120, 100, false, "#0047AB");
 var sevenH = new player(7, "home", 2, 530, 260, 100, false, "#0047AB");
 var eightH = new player(8, "home", 2, 660, 85, 100, false, "#0047AB");
 var nineH = new player(9, "home", 2, 610, 260, 100, false, "#0047AB");
 var tenH = new player(10, "home", 2, 710, 180, 100, false, "#0047AB");
 var elevenH = new player(11, "home", 2, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB");
-var oneG = new player(1, "guest", canvas.width-70, canvas.height / 2 + 10, 100, false, "#FF0000");
+var oneG = new player(1, "guest", 2, 760, 270, 100, false, "#FF0000");
 var twoG = new player(2, "guest", 2, 650, 75, 100, false, "#FF0000");
 var threeG = new player(3, "guest", 2, 625, 270, 100, false, "#FF0000");
 var fourG = new player(4, "guest", 2, 630, 425, 100, false, "#FF0000");
@@ -147,9 +166,9 @@ var sevenG = new player(7, "guest", 2, 315, 85, 100, false, "#FF0000");
 var eightG = new player(8, "guest", 2, 250, 260, 100, false, "#FF0000");
 var nineG = new player(9, "guest", 2, 260, 410, 100, false, "#FF0000");
 var tenG = new player(10, "guest", 2, 140, 130, 100, false, "#FF0000");
-var elvenG = new player(11, "guest", 2, 140, 380, 100, false, "#FF0000");
+var elevenG = new player(11, "guest", 2, 140, 380, 100, false, "#FF0000");
 
-var activePlayers = [oneH, twoH, threeH, fourH, fiveH, sixH, sevenH, eightH, nineH, tenH, elevenH, oneG, twoG, threeG, fourG, fiveG, sixG, sevenG, eightG, nineG, tenG, elvenG];
+var activePlayers = [oneG, twoG, threeG, fourG, fiveG, sixG, sevenG, eightG, nineG, tenG, elevenG];
 
 
 
@@ -298,122 +317,11 @@ function run() {
       }
     };  
     
-    var ball = {
-      x :  canvas.width / 2,
-      y :  canvas.height / 2,
-      //speed: 100,
-      //target : {
-      //  x : 0,
-      //  y : 0
-      //},
-      /*move : function () {
-        const h = Math.sqrt(Math.pow(Math.abs(this.x-this.target.x),2) +  Math.pow(Math.abs(this.y-this.target.y), 2));
-        const v = Math.acos((Math.abs(this.x - this.target.x) / h));
-  
-        const x = ((this.speed/20)+1) * Math.cos(v);
-        const y = ((this.speed/20)+1) * Math.sin(v);
-  
-        this.speed = this.speed * 0.98;
-  
-        if( this.target.x >= this.x && this.target.y >= this.y) {
-            this.setPosition(this.x + x, this.y + y);
-        }
-        else if ( this.target.x >= this.x && this.target.y < this.y) {
-            this.setPosition(this.x + x, this.y - y);
-        }
-        else if ( this.target.x < this.x && this.target.y >= this.y) {
-            this.setPosition(this.x - x, this.y + y);
-        }
-        else if ( this.target.x < this.x && this.target.y < this.y) {
-            this.setPosition(this.x - x, this.y - y);
-        }
-        this.draw();
-      },*/
-      draw : function () {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 3, 0, 2*Math.PI, false);
-        ctx.fillStyle = "#FFFF00";
-        ctx.fill();
-        ctx.strokeStyle = "#000";
-        ctx.stroke();
-        ctx.closePath(); 
-      }  
-    };
     
-    var player = {
-      team: "home",
-      speed: 1.5,
-      x : 10,
-      y : 10,
-      isAt: function (point) {
-        return Math.abs(this.x - point.x) < 1 ? (Math.abs(this.y - point.y) < 1 ? true : false) : false;
-      },
-      move: function (point) {
-        if (!this.isAt(point)) {
-  
-          var h = Math.sqrt(Math.pow(Math.abs(this.x-point.x),2) +  Math.pow(Math.abs(this.y-point.y), 2));
-          var v = Math.acos((Math.abs(this.x - point.x) / h));
-          var x = this.speed * Math.cos(v);
-          var y = this.speed * Math.sin(v);
-  
-          if( point.x >= this.x && point.y >= this.y) {
-            this.x += x;
-            this.y += y;
-          }
-          else if ( point.x >= this.x && point.y < this.y) {
-            this.x += x;
-            this.y -= y;
-          }
-          else if ( point.x < this.x && point.y >= this.y) {
-            this.x -= x;
-            this.y += y;
-          }
-          else if ( point.x < this.x && point.y < this.y) {
-            this.x -= x;
-            this.y -= y;
-          }
-          this.draw();
-        }
-      },
-      draw: function () {
-        pitch.draw();
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 3, 0, 2*Math.PI, false);
-        ctx.fillStyle = ((this.team === "home") ? "#00F" : "#F00");
-        ctx.fill();
-        ctx.strokeStyle = "#000";
-        ctx.stroke();
-        ctx.closePath(); 
-      }
-    };
-    ball.draw();  
-    player.draw();  
-    var coordinates = [
-      { x: 200, y: 259}, 
-      { x: 230, y: 229}, 
-      { x: 290, y: 289}, 
-      { x: 550, y: 289}, 
-      { x: 400, y: 320}, 
-      { x: 200, y: 259}
-    ];
-    
-    var game = {
-      timer: {},
-      step: function () {
-        //player.move(coordinates[0]);
-        ball.move();
-      },
-      start: function () {  
-        pitch.draw();
-        ball.target.x = 800;
-        ball.target.y = 250;
-        this.timer = window.setInterval(this.step, 50);      
-      }
-    };
     pitch.draw();
-    ball.draw();
-    activePlayers.forEach(element => element.draw(ctx));
-    this.timer = window.setInterval(this.step, 50);      
+    playball.draw(ctx);
+    activePlayers.forEach(element => { element.draw(ctx); } );
+      
   })();
   
   
