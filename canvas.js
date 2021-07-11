b2b9df463468958ce1b7f1b6a40ac706b8976dd4
schalliiii <s.schallmaier@gmx.de>
2021-07-11 17:46:08 +0200

    Add Forms

--- canvas.js
+++ canvas.js
@@ -1,8 +1,130 @@
 var canvas = document.getElementById('pitch');
 var ctx = canvas.getContext('2d');
-var power = 1;
+var power = 0.5;
+var anyPossession = false;
+var aimX = 605;
+var aimY = 185;
+
+class pitch {
+  constructor(){
+  }
+
+  draw(ctx){
+    // Outer lines
+    ctx.beginPath();
+    ctx.rect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = "#060";
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#FFF";
+    ctx.stroke();
+    ctx.closePath();
+    
+    ctx.fillStyle = "#FFF";
+    
+    // Mid line
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //Mid circle
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height / 2, 73, 0, 2*(Math.PI), false);
+    ctx.stroke();
+    ctx.closePath();
+    //Mid point
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height / 2, 2, 0, 2*Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+    
+    //Home penalty box
+    ctx.beginPath();
+    ctx.rect(0, (canvas.height - 322) / 2, 132, 322);
+    ctx.stroke();
+    ctx.closePath();
+    //Home goal box
+    ctx.beginPath();
+    ctx.rect(0, (canvas.height - 146) / 2, 44, 146);
+    ctx.stroke();
+    ctx.closePath();
+    //Home goal 
+    ctx.beginPath();
+    ctx.moveTo(1, (canvas.height / 2) - 22);
+    ctx.lineTo(1, (canvas.height / 2) + 22);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.lineWidth = 1;
+
+    //Home penalty point
+    ctx.beginPath()
+    ctx.arc(88, canvas.height / 2, 1, 0, 2*Math.PI, true);
+    ctx.fill();
+    ctx.closePath();
+    //Home half circle
+    ctx.beginPath()
+    ctx.arc(88, canvas.height / 2, 73, 0.29*Math.PI, 1.71*Math.PI, true);
+    ctx.stroke();
+    ctx.closePath();
+    
+    //Away penalty box
+    ctx.beginPath();
+    ctx.rect(canvas.width-132, (canvas.height - 322) / 2, 132, 322);
+    ctx.stroke();
+    ctx.closePath();
+    //Away goal box
+    ctx.beginPath();
+    ctx.rect(canvas.width-44, (canvas.height - 146) / 2, 44, 146);
+    ctx.stroke();
+    ctx.closePath();      
+    //Away goal 
+    ctx.beginPath();
+    ctx.moveTo(canvas.width-1, (canvas.height / 2) - 22);
+    ctx.lineTo(canvas.width-1, (canvas.height / 2) + 22);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.lineWidth = 1;
+    //Away penalty point
+    ctx.beginPath()
+    ctx.arc(canvas.width-88, canvas.height / 2, 1, 0, 2*Math.PI, true);
+    ctx.fill();
+    ctx.closePath();
+    //Away half circle
+    ctx.beginPath()
+    ctx.arc(canvas.width-88, canvas.height / 2, 73, 0.71*Math.PI, 1.29*Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+          
+    //Home L corner
+    ctx.beginPath()
+    ctx.arc(0, 0, 8, 0, 0.5*Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+    //Home R corner
+    ctx.beginPath()
+    ctx.arc(0, canvas.height, 8, 0, 2*Math.PI, true);
+    ctx.stroke();
+    ctx.closePath();
+    //Away R corner
+    ctx.beginPath()
+    ctx.arc(canvas.width, 0, 8, 0.5*Math.PI, 1*Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+    //Away L corner
+    ctx.beginPath()
+    ctx.arc(canvas.width, canvas.height, 8, 1*Math.PI, 1.5*Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+
+}
 class player {
-  constructor(number, team, speed, startingX, startingY, accuracy, possession, trickotColor){
+  constructor(number, team, speed, startingX, startingY, accuracy, possession, trickotColor, name){
       this.number = number;
       this.team = team;
       this.speed = speed;
@@ -13,9 +135,9 @@ class player {
       this.y = startingY;
       this.possession = possession;
       this.trickotColor = trickotColor;
+      this.name = name;
   }
   draw(ctx) {
-    console.log(ctx);
       ctx.beginPath();
       ctx.strokeStyle = this.trickotColor;
       ctx.arc(this.x, this.y, 10, 0, 2*Math.PI, false);
@@ -27,13 +149,13 @@ class player {
   }
 
   isAt(ballX, ballY){
-    this.possession = true;
       return Math.abs(this.x - ballX) < 1 ? (Math.abs(this.y - ballY) < 1 ? true : false) : false;
   }
   
-  move(ballX, ballY, ctx){
+  move(ballX, ballY){
     var bX = ballX,
         bY = ballY;
+        if(Math.abs(bX-this.x) <= 240 || Math.abs(bY-this.y) <= 240){
       if (!this.isAt(bX, bY)) {
         if(bX > this.x){
           this.x = this.x + this.speed;
@@ -47,8 +169,21 @@ class player {
         if(bY < this.Y){
           this.y = this.y - this.speed;
         }
-          this.draw(ctx);
         }
+      } else {
+        if(this.startingX > this.x){
+          this.x = this.x + this.speed;
+        }
+        if(this.startingX < this.X){
+          this.x = this.x - this.speed;
+        }
+        if(this.startingY > this.Y){
+          this.y = this.y + this.speed;
+        }
+        if(this.startingY < this.Y){
+          this.y = this.y - this.speed;
+        }
+      }
   }
 
   getX(){
@@ -73,20 +208,19 @@ class ball{
 
     move(aimX, aimY) {
       if(this.x < aimX){
-        this.x += power;
+        this.x += Math.ceil(power);
       }
       if(this.x > aimX){
-        this.x -= power;
+        this.x -= Math.ceil(power);
       }
       if(this.y < aimY){
-        this.y += power;
+        this.y += Math.ceil(power);
       }
       if(this.y > aimY){
-        this.y -= power;
+        this.y -= Math.ceil(power);
       }
       power = power * 0.98;
 
-      this.draw();
     }
 
     draw(ctx) {
@@ -144,31 +278,44 @@ getY(){
   return this.y;
 }
 }
+var field = new pitch();
 var playball = new ball();
-var oneH = new player(1, "home", 2, 180, 100, 100, false, "#0047AB");
-var twoH = new player(2, "home", 2, 140, 260, 100, false, "#0047AB");
-var threeH = new player(3, "home", 2, 320, 60, false, "#0047AB");
-var fourH = new player(4, "home", 2, 285, 250, 100, false, "#0047AB");
-var fiveH = new player(5, "home", 2, 280, 440, 100, false, "#0047AB");
-var sixH = new player(6, "home", 2, 500, 120, 100, false, "#0047AB");
-var sevenH = new player(7, "home", 2, 530, 260, 100, false, "#0047AB");
-var eightH = new player(8, "home", 2, 660, 85, 100, false, "#0047AB");
-var nineH = new player(9, "home", 2, 610, 260, 100, false, "#0047AB");
-var tenH = new player(10, "home", 2, 710, 180, 100, false, "#0047AB");
-var elevenH = new player(11, "home", 2, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB");
-var oneG = new player(1, "guest", 2, 760, 270, 100, false, "#FF0000");
-var twoG = new player(2, "guest", 2, 650, 75, 100, false, "#FF0000");
-var threeG = new player(3, "guest", 2, 625, 270, 100, false, "#FF0000");
-var fourG = new player(4, "guest", 2, 630, 425, 100, false, "#FF0000");
-var fiveG = new player(5, "guest", 2, 480, 90, 100, false, "#FF0000");
-var sixG = new player(6, "guest", 2, 490, 405, 100, false, "#FF0000");
-var sevenG = new player(7, "guest", 2, 315, 85, 100, false, "#FF0000");
-var eightG = new player(8, "guest", 2, 250, 260, 100, false, "#FF0000");
-var nineG = new player(9, "guest", 2, 260, 410, 100, false, "#FF0000");
-var tenG = new player(10, "guest", 2, 140, 130, 100, false, "#FF0000");
-var elevenG = new player(11, "guest", 2, 140, 380, 100, false, "#FF0000");
+var oneH = new player(1, "home", 2, 180, 100, 100, false, "#0047AB", "Neuer");
+var twoH = new player(2, "home", 2, 140, 260, 100, false, "#0047AB", "Ruediger");
+var threeH = new player(5, "home", 2, 320, 60, 100, false, "#0047AB", "Hummels");
+var fourH = new player(4, "home", 2, 285, 250, 100, false, "#0047AB", "Ginter");
+var fiveH = new player(20, "home", 2, 280, 440, 100, false, "#0047AB", "Gosens");
+var sixH = new player(8, "home", 2, 500, 120, 100, false, "#0047AB", "Kroos");
+var sevenH = new player(21, "home", 2, 530, 260, 100, false, "#0047AB", "Guendogan");
+var eightH = new player(6, "home", 2, 660, 85, 100, false, "#0047AB", "Kimmich");
+var nineH = new player(7, "home", 2, 610, 260, 100, false, "#0047AB", "Harvertz");
+var tenH = new player(25, "home", 2, 710, 180, 100, false, "#0047AB", "Mueller");
+var elevenH = new player(10, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Gnabry");
+var twelveH = new player(3, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Halstenberg");
+var thirteenH = new player(9, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Volland");
+var fourteenH = new player(18, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Goretzka");
+var fiveteenH = new player(22, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Trapp");
+var sixteenH = new player(9, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Werner");
+var oneG = new player(1, "guest", 2, 760, 270, 100, false, "#FF0000", "Patricio");
+var twoG = new player(2, "guest", 2, 650, 75, 100, false, "#FF0000", "Semedo");
+var threeG = new player(3, "guest", 2, 625, 270, 100, false, "#FF0000", "Pepe");
+var fourG = new player(4, "guest", 2, 630, 425, 100, false, "#FF0000", "Dias");
+var fiveG = new player(5, "guest", 2, 480, 90, 100, false, "#FF0000", "Guerreiro");
+var sixG = new player(13, "guest", 2, 490, 405, 100, false, "#FF0000", "Pereira");
+var sevenG = new player(10, "guest", 2, 315, 85, 100, false, "#FF0000", "Silva");
+var eightG = new player(11, "guest", 2, 250, 260, 100, false, "#FF0000", "Fernandes");
+var nineG = new player(14, "guest", 2, 260, 410, 100, false, "#FF0000", "Carvalho");
+var tenG = new player(21, "guest", 2, 140, 130, 100, false, "#FF0000", "Jota");
+var elevenG = new player(7, "guest", 2, 140, 380, 100, false, "#FF0000", "Ronaldo");
+var twelveG = new player(19, "guest", 2, 140, 380, 100, false, "#FF0000", "Goncalves");
+var thirteenG = new player(12, "guest", 2, 140, 380, 100, false, "#FF0000", "Lopes");
+var fourteenG = new player(20, "guest", 2, 140, 380, 100, false, "#FF0000", "Dalot");
+var fiveteenG = new player(16, "guest", 2, 140, 380, 100, false, "#FF0000", "Sanches");
+var sixteenG = new player(18, "guest", 2, 140, 380, 100, false, "#FF0000", "Neves");
 
-var activePlayers = [oneG, twoG, threeG, fourG, fiveG, sixG, sevenG, eightG, nineG, tenG, elevenG];
+var activePlayers = [oneH, twoH, threeH, fourH, fiveH, sixH, sevenH, eightH, nineH, tenH, elevenH, oneG, twoG, threeG, fourG, fiveG, sixG, sevenG, eightG, nineG, tenG, elevenG];
+var substitutesH = [twelveH, thirteenH, fourteenH, fiveteenH, sixteenH];
+var substitutesG = [twelveG, thirteenG, fourteenG, fiveteenG, sixteenG];
 
 
 
@@ -192,136 +339,62 @@ canvas.addEventListener("mousedown", function(e)
   getMousePosition(canvas, e);
 });
 
-function run() {
+
+function play(){
+  while(!anyPossession){
+  activePlayers.forEach(element => {
+    if (element.isAt(ball.getX, ball.getY)){
+      element.possession = true;
+      playball.setX(element.x);
+      playball.setY(element.y);
+      anyPossession = true;
+      return
+    }
+  })
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    field.draw(ctx);
+    playball.move(aimX, aimY);
+    playball.draw(ctx);
+    activePlayers.forEach(element => {
+      element.move(playball.getX(), playball.getY(), ctx);
+      element.draw(ctx);
+    })
   
 }
 
 
+}
 
+function test() {
+  for(var i = 0; i < 500; i++){
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    field.draw(ctx);
+    playball.move(aimX, aimY);
+    playball.draw(ctx);
+    activePlayers.forEach(element => {
+      element.move(playball.getX(), playball.getY());
+      element.draw(ctx);
+    });
+    setTimeout(function(){ console.log("hi"); }, 3000);
+  }
+}
 
 (function () {  
-
-    
-    var pitch = {
-      draw : function () {
-        
-        // Outer lines
-        ctx.beginPath();
-        ctx.rect(0,0, canvas.width, canvas.height);
-        ctx.fillStyle = "#060";
-        ctx.fill();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "#FFF";
-        ctx.stroke();
-        ctx.closePath();
-        
-        ctx.fillStyle = "#FFF";
-        
-        // Mid line
-        ctx.beginPath();
-        ctx.moveTo(canvas.width / 2, 0);
-        ctx.lineTo(canvas.width / 2, canvas.height);
-        ctx.stroke();
-        ctx.closePath();
-        
-        //Mid circle
-        ctx.beginPath()
-        ctx.arc(canvas.width / 2, canvas.height / 2, 73, 0, 2*(Math.PI), false);
-        ctx.stroke();
-        ctx.closePath();
-        //Mid point
-        ctx.beginPath()
-        ctx.arc(canvas.width / 2, canvas.height / 2, 2, 0, 2*Math.PI, false);
-        ctx.fill();
-        ctx.closePath();
-        
-        //Home penalty box
-        ctx.beginPath();
-        ctx.rect(0, (canvas.height - 322) / 2, 132, 322);
-        ctx.stroke();
-        ctx.closePath();
-        //Home goal box
-        ctx.beginPath();
-        ctx.rect(0, (canvas.height - 146) / 2, 44, 146);
-        ctx.stroke();
-        ctx.closePath();
-        //Home goal 
-        ctx.beginPath();
-        ctx.moveTo(1, (canvas.height / 2) - 22);
-        ctx.lineTo(1, (canvas.height / 2) + 22);
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.closePath();
-        ctx.lineWidth = 1;
-  
-        //Home penalty point
-        ctx.beginPath()
-        ctx.arc(88, canvas.height / 2, 1, 0, 2*Math.PI, true);
-        ctx.fill();
-        ctx.closePath();
-        //Home half circle
-        ctx.beginPath()
-        ctx.arc(88, canvas.height / 2, 73, 0.29*Math.PI, 1.71*Math.PI, true);
-        ctx.stroke();
-        ctx.closePath();
-        
-        //Away penalty box
-        ctx.beginPath();
-        ctx.rect(canvas.width-132, (canvas.height - 322) / 2, 132, 322);
-        ctx.stroke();
-        ctx.closePath();
-        //Away goal box
-        ctx.beginPath();
-        ctx.rect(canvas.width-44, (canvas.height - 146) / 2, 44, 146);
-        ctx.stroke();
-        ctx.closePath();      
-        //Away goal 
-        ctx.beginPath();
-        ctx.moveTo(canvas.width-1, (canvas.height / 2) - 22);
-        ctx.lineTo(canvas.width-1, (canvas.height / 2) + 22);
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.closePath();
-        ctx.lineWidth = 1;
-        //Away penalty point
-        ctx.beginPath()
-        ctx.arc(canvas.width-88, canvas.height / 2, 1, 0, 2*Math.PI, true);
-        ctx.fill();
-        ctx.closePath();
-        //Away half circle
-        ctx.beginPath()
-        ctx.arc(canvas.width-88, canvas.height / 2, 73, 0.71*Math.PI, 1.29*Math.PI, false);
-        ctx.stroke();
-        ctx.closePath();
-              
-        //Home L corner
-        ctx.beginPath()
-        ctx.arc(0, 0, 8, 0, 0.5*Math.PI, false);
-        ctx.stroke();
-        ctx.closePath();
-        //Home R corner
-        ctx.beginPath()
-        ctx.arc(0, canvas.height, 8, 0, 2*Math.PI, true);
-        ctx.stroke();
-        ctx.closePath();
-        //Away R corner
-        ctx.beginPath()
-        ctx.arc(canvas.width, 0, 8, 0.5*Math.PI, 1*Math.PI, false);
-        ctx.stroke();
-        ctx.closePath();
-        //Away L corner
-        ctx.beginPath()
-        ctx.arc(canvas.width, canvas.height, 8, 1*Math.PI, 1.5*Math.PI, false);
-        ctx.stroke();
-        ctx.closePath();
-      }
-    };  
-    
-    
-    pitch.draw();
+     
+    field.draw(ctx);
     playball.draw(ctx);
     activePlayers.forEach(element => { element.draw(ctx); } );
-      
+    //play();
+/*     for(var i = 0; i < 300; i++){
+      ctx.clearRect(0, 0, innerWidth, innerHeight);
+      field.draw(ctx);
+      playball.move(aimX, aimY);
+      playball.draw(ctx);
+      elevenH.move(playball.getX(), playball.getY());
+      elevenH.draw(ctx);
+      console.log(playball.getX(), playball.getY());
+    }  */
+
   })();
   
   
