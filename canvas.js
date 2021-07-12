05c6215dea08af64783725fe2f0feca040b2b45c
schalliiii <s.schallmaier@gmx.de>
2021-07-12 22:28:43 +0200

    add substitutions

--- canvas.js
+++ canvas.js
@@ -4,6 +4,121 @@ var power = 0.5;
 var anyPossession = false;
 var aimX = 605;
 var aimY = 185;
+var homePlayerSelect = document.getElementById('selectHomePlayer');
+var guestPlayerSelect = document.getElementById('selectGuestPlayer');
+var formsH = document.getElementById("formsH");
+var formsG = document.getElementById("formsG");
+var selectHomeSub = document.getElementById("selectHomeSub");
+var selectGuestSub = document.getElementById("selectGuestSub");
+var subListH = document.getElementById("selectHomeSub");
+var subListG = document.getElementById("selectGuestSub");
+var confirmSubH = document.getElementById("confirmSubH");
+var confirmSubG = document.getElementById("confirmSubG");
+
+
+formsH.style.display = "none";
+formsG.style.display = "none";
+selectHomeSub.style.display = "none";
+selectGuestSub.style.display = "none";
+confirmSubH.style.display = "none";
+confirmSubG.style.display = "none";
+
+function redraw() {
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  field.draw(ctx);
+  playball.draw(ctx);
+  activePlayers.forEach(element => { element.draw(ctx); } );
+}
+
+function emptySelect(box) {
+var length = box.options.length;
+for (i = length-1; i >= 0; i--) {
+  box.options[i] = null;
+}
+}
+
+function showHomeSub(){
+  selectHomeSub.style.display = "block";
+  confirmSubH.style.display = "block";
+}
+
+function showGuestSub(){
+  selectGuestSub.style.display = "block";
+  confirmSubG.style.display = "block";
+}
+
+function confirmSubHome() {
+  var e = document.getElementById("selectHomePlayer").value;
+  var f = document.getElementById("selectHomeSub").value;
+  var x;
+  var y;
+  for(var i = 0; i < activeH.length; i++){
+    if(activeH[i].name == e){
+      substitutesH.push(activeH[i]);
+      x = activeH[i].x;
+      y = activeH[i].y;
+      activeH.splice(i, 1);
+      for(var y = 0; y < activePlayers.length; y++){
+        if(activePlayers[y].name == e){
+          activePlayers.splice(i, 1);
+        }
+      }
+    }
+  }
+  for(var i = 0; i < substitutesH.length; i++){
+    if(substitutesH[i].name == f){
+      substitutesH[i].x = x;
+      substitutesH[i].y = y;
+      activeH.push(substitutesH[i]);
+      activePlayers.push(substitutesH[i]);
+      substitutesH.splice(i, 1);
+    }
+  }
+  emptySelect(homePlayerSelect);
+  emptySelect(selectHomeSub);
+  fillActiveHome();
+  fillSubHome();
+  redraw();
+  confirmSubH.style.display="none";
+  selectHomeSub.style.display="none";
+}
+
+function confirmSubGuest() {
+  var e = document.getElementById("selectGuestPlayer").value;
+  var f = document.getElementById("selectGuestSub").value;
+  var x;
+  var y;
+  for(var i = 0; i < activeG.length; i++){
+    if(activeG[i].name == e){
+      substitutesH.push(activeG[i]);
+      x = activeG[i].x;
+      y = activeG[i].y;
+      activeG.splice(i, 1);
+      for(var y = 0; y < activePlayers.length; y++){
+        if(activePlayers[y].name == e){
+          activePlayers.splice(i, 1);
+        }
+      }
+    }
+  }
+  for(var i = 0; i < substitutesG.length; i++){
+    if(substitutesG[i].name == f){
+      substitutesG[i].x = x;
+      substitutesG[i].y = y;
+      activeG.push(substitutesG[i]);
+      activePlayers.push(substitutesG[i]);
+      substitutesG.splice(i, 1);
+    }
+  }
+  emptySelect(guestPlayerSelect);
+  emptySelect(selectGuestSub);
+  fillActiveHome();
+  fillSubHome();
+  redraw();
+  confirmSubG.style.display="none";
+  selectGuestSub.style.display="none";
+}
+
 
 class pitch {
   constructor(){
@@ -280,42 +395,92 @@ getY(){
 }
 var field = new pitch();
 var playball = new ball();
-var oneH = new player(1, "home", 2, 180, 100, 100, false, "#0047AB", "Neuer");
-var twoH = new player(2, "home", 2, 140, 260, 100, false, "#0047AB", "Ruediger");
-var threeH = new player(5, "home", 2, 320, 60, 100, false, "#0047AB", "Hummels");
-var fourH = new player(4, "home", 2, 285, 250, 100, false, "#0047AB", "Ginter");
-var fiveH = new player(20, "home", 2, 280, 440, 100, false, "#0047AB", "Gosens");
-var sixH = new player(8, "home", 2, 500, 120, 100, false, "#0047AB", "Kroos");
-var sevenH = new player(21, "home", 2, 530, 260, 100, false, "#0047AB", "Guendogan");
-var eightH = new player(6, "home", 2, 660, 85, 100, false, "#0047AB", "Kimmich");
-var nineH = new player(7, "home", 2, 610, 260, 100, false, "#0047AB", "Harvertz");
-var tenH = new player(25, "home", 2, 710, 180, 100, false, "#0047AB", "Mueller");
-var elevenH = new player(10, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Gnabry");
-var twelveH = new player(3, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Halstenberg");
-var thirteenH = new player(9, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Volland");
-var fourteenH = new player(18, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Goretzka");
-var fiveteenH = new player(22, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Trapp");
-var sixteenH = new player(9, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Werner");
-var oneG = new player(1, "guest", 2, 760, 270, 100, false, "#FF0000", "Patricio");
-var twoG = new player(2, "guest", 2, 650, 75, 100, false, "#FF0000", "Semedo");
-var threeG = new player(3, "guest", 2, 625, 270, 100, false, "#FF0000", "Pepe");
-var fourG = new player(4, "guest", 2, 630, 425, 100, false, "#FF0000", "Dias");
-var fiveG = new player(5, "guest", 2, 480, 90, 100, false, "#FF0000", "Guerreiro");
-var sixG = new player(13, "guest", 2, 490, 405, 100, false, "#FF0000", "Pereira");
-var sevenG = new player(10, "guest", 2, 315, 85, 100, false, "#FF0000", "Silva");
-var eightG = new player(11, "guest", 2, 250, 260, 100, false, "#FF0000", "Fernandes");
-var nineG = new player(14, "guest", 2, 260, 410, 100, false, "#FF0000", "Carvalho");
-var tenG = new player(21, "guest", 2, 140, 130, 100, false, "#FF0000", "Jota");
-var elevenG = new player(7, "guest", 2, 140, 380, 100, false, "#FF0000", "Ronaldo");
-var twelveG = new player(19, "guest", 2, 140, 380, 100, false, "#FF0000", "Goncalves");
-var thirteenG = new player(12, "guest", 2, 140, 380, 100, false, "#FF0000", "Lopes");
-var fourteenG = new player(20, "guest", 2, 140, 380, 100, false, "#FF0000", "Dalot");
-var fiveteenG = new player(16, "guest", 2, 140, 380, 100, false, "#FF0000", "Sanches");
-var sixteenG = new player(18, "guest", 2, 140, 380, 100, false, "#FF0000", "Neves");
+var Neuer = new player(1, "home", 2, 180, 100, 100, false, "#0047AB", "Neuer");
+var Ruediger = new player(2, "home", 2, 140, 260, 100, false, "#0047AB", "Ruediger");
+var Hummels = new player(5, "home", 2, 320, 60, 100, false, "#0047AB", "Hummels");
+var Ginter = new player(4, "home", 2, 285, 250, 100, false, "#0047AB", "Ginter");
+var Gosens = new player(20, "home", 2, 280, 440, 100, false, "#0047AB", "Gosens");
+var Kroos = new player(8, "home", 2, 500, 120, 100, false, "#0047AB", "Kroos");
+var Guendogan = new player(21, "home", 2, 530, 260, 100, false, "#0047AB", "Guendogan");
+var Kimmich = new player(6, "home", 2, 660, 85, 100, false, "#0047AB", "Kimmich");
+var Harvertz = new player(7, "home", 2, 610, 260, 100, false, "#0047AB", "Harvertz");
+var Mueller = new player(25, "home", 2, 710, 180, 100, false, "#0047AB", "Mueller");
+var Gnabry = new player(10, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Gnabry");
+var Halstenberg = new player(3, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Halstenberg");
+var Volland = new player(9, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Volland");
+var Goretzka = new player(18, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Goretzka");
+var Trapp = new player(22, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Trapp");
+var Werner = new player(11, "home", 0.5, canvas.width / 2 + 10 , canvas.height / 2 + 10, 100, true, "#0047AB", "Werner");
+var Patricio = new player(1, "guest", 2, 760, 270, 100, false, "#FF0000", "Patricio");
+var Semedo = new player(2, "guest", 2, 650, 75, 100, false, "#FF0000", "Semedo");
+var Pepe = new player(3, "guest", 2, 625, 270, 100, false, "#FF0000", "Pepe");
+var Dias = new player(4, "guest", 2, 630, 425, 100, false, "#FF0000", "Dias");
+var Guerreiro = new player(5, "guest", 2, 480, 90, 100, false, "#FF0000", "Guerreiro");
+var Pereira = new player(13, "guest", 2, 490, 405, 100, false, "#FF0000", "Pereira");
+var Silva = new player(10, "guest", 2, 315, 85, 100, false, "#FF0000", "Silva");
+var Fernandes = new player(11, "guest", 2, 250, 260, 100, false, "#FF0000", "Fernandes");
+var Carvalho = new player(14, "guest", 2, 260, 410, 100, false, "#FF0000", "Carvalho");
+var Jota = new player(21, "guest", 2, 140, 130, 100, false, "#FF0000", "Jota");
+var Ronaldo = new player(7, "guest", 2, 140, 380, 100, false, "#FF0000", "Ronaldo");
+var Goncalves = new player(19, "guest", 2, 140, 380, 100, false, "#FF0000", "Goncalves");
+var Lopes = new player(12, "guest", 2, 140, 380, 100, false, "#FF0000", "Lopes");
+var Dalot = new player(20, "guest", 2, 140, 380, 100, false, "#FF0000", "Dalot");
+var Sanches = new player(16, "guest", 2, 140, 380, 100, false, "#FF0000", "Sanches");
+var Neves = new player(18, "guest", 2, 140, 380, 100, false, "#FF0000", "Neves");
 
-var activePlayers = [oneH, twoH, threeH, fourH, fiveH, sixH, sevenH, eightH, nineH, tenH, elevenH, oneG, twoG, threeG, fourG, fiveG, sixG, sevenG, eightG, nineG, tenG, elevenG];
-var substitutesH = [twelveH, thirteenH, fourteenH, fiveteenH, sixteenH];
-var substitutesG = [twelveG, thirteenG, fourteenG, fiveteenG, sixteenG];
+var activePlayers = [Neuer, Ruediger, Hummels, Ginter, Gosens, Kroos, Guendogan, Kimmich, Harvertz, Mueller, Gnabry, Patricio, Semedo, Pepe, Dias, Guerreiro, Pereira, Silva, Fernandes, Carvalho, Jota, Ronaldo];
+var activeH = [Neuer, Ruediger, Hummels, Ginter, Gosens, Kroos, Guendogan, Kimmich, Harvertz, Mueller, Gnabry];
+var activeG = [Patricio, Semedo, Pepe, Dias, Guerreiro, Pereira, Silva, Fernandes, Carvalho, Jota, Ronaldo];
+var substitutesH = [Halstenberg, Volland, Goretzka, Trapp, Werner];
+var substitutesG = [Goncalves, Lopes, Dalot, Sanches, Neves];
+
+
+function fillActiveHome() {
+  for(var i = 0; i < activeH.length; i++) {
+    var hplayer = activeH[i].name;
+    var el = document.createElement("option");
+    el.textContent = hplayer;
+    el.value = hplayer;
+    homePlayerSelect.appendChild(el);
+  };
+}
+
+
+function fillActiveGuest() {
+  for(var i = 0; i < activeG.length; i++) {
+    var gplayer = activeG[i].name;
+    var el = document.createElement("option");
+    el.textContent = gplayer;
+    el.value = gplayer;
+    guestPlayerSelect.appendChild(el);
+  };
+}
+
+
+function fillSubHome() {
+  for(var i = 0; i < substitutesH.length; i++) {
+    var shplayer = substitutesH[i].name;
+    var el = document.createElement("option");
+    el.textContent = shplayer;
+    el.value = shplayer;
+    subListH.appendChild(el);
+  };
+}
+
+
+function fillSubGuest() {
+  for(var i = 0; i < substitutesG.length; i++) {
+    var sgplayer = substitutesG[i].name;
+    var el = document.createElement("option");
+    el.textContent = sgplayer;
+    el.value = sgplayer;
+    subListG.appendChild(el);
+  };  
+}
+fillActiveHome();
+fillActiveGuest();
+fillSubHome();
+fillSubGuest();
 
 
 
